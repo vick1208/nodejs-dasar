a9dfc9fe9a2a2705d06e58996ce0d313e34b6544
Vicky AS <vickyhuang808@gmail.com>
2024-05-13 21:44:23 +0700

    DNS edit

--- dns.mjs
+++ dns.mjs
@@ -1,6 +1,6 @@
 import dns from "dns/promises";
 
-const address = await dns.lookup("github.com");
+const address = await dns.lookup("www.programmerzamannow.com");
 
 console.info(address.address);
 console.info(address.family);
